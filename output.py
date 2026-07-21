f40b4ed82753cd61bce8919d585c5ff5d0376a6b
sanjana <sanjanasampath1008@gmail.com>
2026-07-21 18:40:36 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,21 @@
+Words:
+
+['AI', 'makes', 'learning', 'easy']
+
+
+Embedding Vectors:
+[0.3, 0.2, 0.5]
+
+[0.6, 0.4, 0.3]
+
+[0.8, 0.7, 0.6]
+
+[0.5, 0.6, 0.9]
+
+
+Attention Weights:
+[0.25, 0.2, 0.35, 0.2]
+
+
+Final Attention Output:
+[0.575, 0.495, 0.5750000000000001]
